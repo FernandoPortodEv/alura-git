f0b600ac738c7c570a315443a72923c40436236f
Fernando Porto <fernandogomes143@gmail.com>
2022-01-09 21:59:18 -0300

    Alterado bugs nas váriaveis e funções

--- Peso_Categoria.java
+++ Peso_Categoria.java
@@ -1,12 +1,12 @@
 public class Peso_Categoria {
-    public static void main(String args) {
+    public static void main(String[] args) {
 
-        double peso=92.5;
+        double peso = 92.5;
             System.out.println("Atualmente seu peso é " + peso);
         if(peso<100) {
             System.out.println("Parabéns, você está enquadrado nessa categoria, boa sorte!");
             }
-            else(peso>=100) {
+            else {
             System.out.println("Poxa, infelizmente você não se enquadra nessa categoria");
         }    
     }
